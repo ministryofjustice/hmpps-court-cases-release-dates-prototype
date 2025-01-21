9a2a463ea8f0cc5bffbdec0767a7e6c250b2cc5e
jamesmckechnie <34271054+jamesmckechnie@users.noreply.github.com>
2025-01-21 08:28:54 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -2257,7 +2257,7 @@ router.get("/:prototypeVersion/launch-prototype", function (req, res) {
     req.session.data = sessionDataDefaults;
     req.session.data.prototypeVersion = prototypeVersion;
     console.log("Launching prototype version: " + prototypeVersion);
-    res.redirect(`/${prototypeVersion}/dps-home`);
+    res.redirect(`/${prototypeVersion}/DPS-home`);
   });
 });
 
